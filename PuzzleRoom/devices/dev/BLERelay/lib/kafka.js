db92689b4c57621caaebf619221525f4153c6716
Ben <ben.smith@robomq.io>
2017-05-02 21:05:16 -0400

    Updated node.js app code and Docker files

--- PuzzleRoom/devices/dev/BLERelay/lib/kafka.js
+++ PuzzleRoom/devices/dev/BLERelay/lib/kafka.js
@@ -1,3 +1,5 @@
+/*eslint-env node*/
+/*eslint no-console:["error", { allow: ["info", "error"] }]*/
 const Kafka = require('node-rdkafka');
 const jsonfile = require('jsonfile');
 var configData = null;
@@ -5,45 +7,45 @@ var configData = null;
 var KafkaInit = function (filePath) {
 	jsonfile.readFile(filePath, function(err, fileobj){
 		if (err != null){
-			console.log('Failed to load the config file for the producer. Details:\n\t' + err);
+			console.error('Failed to load the config file for the producer. Details:\n\t' + err);
 			process.exit(1);
 		}
 		configData = fileobj;
-	}
-}
+	});
+};
 
 var addProducer = function (logger, configKey){
 	
 	if (configData === null) {
-		logger.error("Kafka client has not loaded its config file");
+		logger.error('Kafka client has not loaded its config file');
 		process.exit(1);
 	}
 
 	if (!(configData.hasOwnProperty(configKey))) {
-		logger.error("The Key: " + configKey + " is not in the loaded config file");
+		logger.error('The Key: ' + configKey + ' is not in the loaded config file');
 		process.exit(1);
 	}
 
 	try {
 		var producer = new Kafka.Producer(configData[configKey]);
-		logger.info("Producer is connecting to the Kafka broker");
+		logger.info('Producer is connecting to the Kafka broker');
 		
 		producer.connect();
 		producer.getMetaData(opt, function (err, metaData) {
 			if (err != null) {
-				logger.error("Failed to query metaData");
+				logger.error('Failed to query metaData');
 				process.exit(1);
 			} else {
-			logger.info("Producer is successfully connected to the" + metaData.orig_broker_name + "Kafka broker");
+			logger.info('Producer is successfully connected to the' + metaData.orig_broker_name + 'Kafka broker');
 			}
 		});
 		
 		producer.on('event.error', function(err) {
-			logger.error("Producer encountered an error. Details\n\t"+err);
+			logger.error('Producer encountered an error. Details\n\t'+err);
 		});
 		
 		producer.on('ready', function() {
-			logger.info("Producer is ready to publish messages.");
+			logger.info('Producer is ready to publish messages.');
 			return producer;
 		});
 	} catch (err) {
@@ -52,39 +54,39 @@ var addProducer = function (logger, configKey){
 	}
 }
 
-var addConsumer = function (logger, config){
+var addConsumer = function (logger, configKey){
 	if (configData === null) {
-		logger.error("Kafka client hs not loaded its config file");
+		logger.error('Kafka client hs not loaded its config file');
 		process.exit(1);
 	}
 
 	if (!(configData.hasOwnProperty(configKey))) {
-		logger.error("The Key: " + configKey + " is not in the loaded config file");
+		logger.error('The Key: ' + configKey + ' is not in the loaded config file');
 		process.exit(1);
 	}
 
 	try {
 		var consumer = new Kafka.Consumer(configData[config],{});
-		logger.info("Consumer is connecting to the Kafka broker");
+		logger.info('Consumer is connecting to the Kafka broker');
 		
 		consumer.connect();
 		consumer.getMetaData(opt, function (err, metaData) {
 			if (err != null) {
-				logger.error("Failed to query metaData");
+				logger.error('Failed to query metaData');
 				process.exit(1);
 			} else {
-			logger.info("Consumer is successfully connected to the" + metaData.orig_broker_name + "Kafka broker");
+			logger.info('Consumer is successfully connected to the' + metaData.orig_broker_name + 'Kafka broker');
 			}
 		});
 
 		consumer.on('event.error', function(err) {
-			logger.error("Consumer encountered an error. Details\n\t"+err);
+			logger.error('Consumer encountered an error. Details\n\t'+err);
 		});
 
 		consumer.on('ready', function() {
 			consumer.subscribe([configData[config]['Topic']]);
 			consumer.consume();
-			logger.info("Consumer is ready to consume messages. On topic "+ configData[config]['Topic'] +".");
+			logger.info('Consumer is ready to consume messages. On topic '+ configData[config]['Topic'] +'.');
 			return consumer;
 		});
 	} catch (err) {
