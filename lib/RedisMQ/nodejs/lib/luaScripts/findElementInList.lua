371b6828985c10bf1a2a739015851f100fca3413
Ben <ben.smith@robomq.io>
2017-08-05 16:35:59 -0400

    Promisified functions for the intialization of the publisher

--- lib/RedisMQ/nodejs/lib/luaScripts/findElementInList.lua
+++ lib/RedisMQ/nodejs/lib/luaScripts/findElementInList.lua
@@ -2,28 +2,28 @@
 
   Desc:
     This method is used to find a specific element in a list and return a 1 or 0 depending on if the the element exists in the list
-  Args:
+  Input:
     Key:
       (1) List (String): The List that is being sifted through.
-      (2) Element (String): The element being looked for.
+    Args:
+      (1) Element (String): The element being looked for.
   Returns:
-    0 (Int): If the the address doesn't exist in the List
-    1 (Int): If the the address exists in the List
-    nil (Null): If the 'LLEN' or 'LINDEX' returns a nil if the List doesn't exist or the index that is being looked at doesn't exist in the list.
-
+    0 (Int): If Argument one doesn't exist in the List.
+    1 (Int): If Argument one exists in the List.
+    -1 (Int): If the List that exists at the key has a length of zero.
+    nil (Null): If the key that is passed in doesn't exist or is not a List.
 --]]
-local length = redis.call("LLEN", KEYS[0]) - 1
-if length == nil then
+if redis.call("TYPE", KEYS[1])["ok"] ~= 'list' then
   return nil
-else
-  for i=length,0,-1 do
-    local address = redis.call("LINDEX", KEYS[0], i)
-    if address == nil then
-      return nil
-    end
-    if address == ARGV[0] then
-      return 1
-    end
-  end
-  return 0
 end
+local length = redis.call("LLEN", KEYS[1])
+if length == 0 then
+  return -1
+end
+for i=-1,-length,-1 do
+  local address = redis.call("LINDEX", KEYS[1], i)
+  if address == ARGV[1] then
+    return 1
+  end
+end
+return 0
