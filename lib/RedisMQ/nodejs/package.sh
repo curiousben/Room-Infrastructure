e5ddae60de0dd8369112b97c073ecd3a3ddc5128
Ben <ben.smith@robomq.io>
2017-08-10 00:01:10 -0400

    Exposed the hashed lua scripts on main obj

--- lib/RedisMQ/nodejs/package.sh
+++ lib/RedisMQ/nodejs/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "---INFO: Packaging tarball"
-cd ../ && tar -czvf node.redis.mq.tar.gz --exclude config --exclude package.sh --exclude package.json --exclude bin --exclude node_modules --exclude config nodejs/
+cd ../ && tar -czvf node.redis.mq.tar.gz  --exclude .DS_Store --exclude config --exclude package.sh --exclude package.json --exclude bin --exclude node_modules --exclude config nodejs/
 if [ $? -ne 0 ]
 then
     echo "----ERROR: Failed to create tarball"
