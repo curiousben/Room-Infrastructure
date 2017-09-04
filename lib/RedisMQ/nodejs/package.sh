d50cf5744797b41f472455a8d07b8e344782c8b5
Ben <Smith@BensMacPro.local>
2017-09-04 13:26:19 -0400

    Created a dockerfile that will house a ready to go RedisMQ library

--- lib/RedisMQ/nodejs/package.sh
+++ lib/RedisMQ/nodejs/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "---INFO: Packaging tarball"
-cd ../ && tar -czvf node.redis.mq.tar.gz  --exclude test --exclude .DS_Store --exclude config --exclude package.sh --exclude package.json --exclude bin --exclude node_modules --exclude config nodejs/
+cd ../ && tar -czvf node.redis.mq.tar.gz  --exclude test --exclude .DS_Store --exclude config --exclude package.sh --exclude bin --exclude node_modules --exclude config --exclude docker/ nodejs/
 if [ $? -ne 0 ]
 then
     echo "----ERROR: Failed to create tarball"
@@ -9,7 +9,7 @@ then
 fi
 
 echo "----INFO: Moving tarball to the dist folder"
-mv node.redis.mq.tar.gz dist/
+mv node.redis.mq.tar.gz nodejs/docker
 if [ $? -ne 0 ]
 then
     echo "----ERROR: Failed to move tarball to dist folder"
