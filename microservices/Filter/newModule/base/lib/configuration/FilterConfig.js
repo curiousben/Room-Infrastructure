43a5e3a3fa1127db70c1033c31757908c48d4dea
curiousben <benjamindsmith3@gmail.com>
2018-09-09 20:26:51 -0400

    Continued to refactor the Filter microservice

--- microservices/Filter/newModule/base/lib/configuration/FilterConfig.js
+++ microservices/Filter/newModule/base/lib/configuration/FilterConfig.js
@@ -1,17 +1,20 @@
 'user strict'
 
 /*
-*
-*
-*
-*
+* This class loads the Filter configuration and validates the confiuration
+* @Author: Ben Smith
+* @Date: Sept 9th, 2018
 */
 
 // 'Private' class variables
 const _logger = Symbol('logger')
 const _jsonObj = Symbol('JSONObj')
 const _filterRules = Symbol('FilterRules')
-const _throwsException = Symbol('ThrowsException')
+const _shouldThrowError = Symbol('shouldThrowError')
+
+// 'Private' class methods
+const _validateShouldThrowError = Symbol('validateShouldThrowError')
+const _validateFilterRule = Symbol('validateFilterRule')
 
 class FilterConfig {
 
@@ -22,60 +25,73 @@ class FilterConfig {
   }
 
   async loadConfigurations() {
-    for (const filterRule of this[_jsonObj]){
+    // Validate shouldThrowError Configuration
+    this[_shouldThrowError] = await [_validateShouldThrowError](this[_jsonObj])
+    const filterRules = this[_jsonObj]['filterRules']
+
+    // Validate filterRules
+    this[_filterRules] = []
+    for (const filterRule of filterRules){
       this[_filterRules].add(await this[_validateThrowErrorConfig](this[_jsonObj]))
     }
   }
 
-  async [_validateFilterRule] (jsonObj) {
+  async [_validateShouldThrowError](jsonObj) {
+    const shouldThrowError= jsonObj['shouldThrowError']
+    if (typeof throwsError !== 'boolean') {
+      const errorDesc = `A Filter rule for the key ${key} has encountered '${shouldThrowError}' for the throwsError. ThrowsError can only be a Boolean.`
+      this[_logger].error(errorDesc)
+      throw new Error(errorDesc)
+    }
+    return shouldThrowError
+  }
+
+  async [_validateFilterRule](filterRule) {
 
     // Checking the surface level configuration for existence and they are the arrays
-    if (!('key' in jsonObj ) || !('acceptedValues' in jsonObj) || !('location' in jsonObj) || !('typeOfMatch' in jsonObj) || !('throwsError' in jsonObj)) {
-      const errorDesc = `Filter configuration is missing either 'key', 'acceptedValues', 'location', 'typeOfMatch', or 'throwsError' for the filtered data ${jsonObj}.`
+    if (!('key' in filterRule ) || !('acceptedValues' in filterRule) || !('location' in filterRule) || !('typeOfMatch' in filterRule)) {
+      const errorDesc = `Filter configuration is missing either 'key', 'acceptedValues', 'location', 'typeOfMatch', or 'throwsError' for the filtered data ${filterRule}.`
       this[_logger].error(errorDesc)
       throw new Error(errorDesc)
     }
 
-    const key = jsonObj['key']
+    const key = filterRule['key']
     if (typeof typeOfMatch !== 'string' && !(typeOfMatch instanceof String)) {
       const errorDesc = `A Filter rule for the key ${key} is not a string. The Key configuration can only be String.`
       this[_logger].error(errorDesc)
       throw new Error(errorDesc)
     }
 
-    const acceptedValues = jsonObj['acceptedValues']
+    const acceptedValues = filterRule['acceptedValues']
     if (typeof acceptedValues !== 'object' || acceptedValues.constructor !== Array) {
       const errorDesc = `A Filter rule for the key ${key} has encountered '[${acceptedValues.join(', ')}]' for the acceptedValues. AcceptedValues can only be an Array.`
       this[_logger].error(errorDesc)
       throw new Error(errorDesc)
     }
 
-    const locationOfData = jsonObj['location']
+    const locationOfData = filterRule['location']
     if (typeof locationOfData !== 'object' || locationOfData.constructor !== Array) {
       const errorDesc = `A Filter rule for the key ${key} has encountered '[${locationOfData.join(', ')}]' for the locationOfData. LocationOfData can only be an Array.`
       this[_logger].error(errorDesc)
       throw new Error(errorDesc)
     }
 
-    const typeOfMatch = jsonObj['typeOfMatch']
+    const typeOfMatch = filterRule['typeOfMatch']
     if (typeof typeOfMatch !== 'string' && !(typeOfMatch instanceof String)) {
       const errorDesc = `A Filter rule for the key ${key} has encountered '[${typeOfMatch.join(', ')}]' for the typeOfMatch .TypeOfMatch can only be a String.`
       this[_logger].error(errorDesc)
       throw new Error(errorDesc)
     }
 
-    const throwsError = jsonObj['throwsError']
-    if (typeof throwsError !== 'boolean') {
-      const errorDesc = `A Filter rule for the key ${key} has encountered '${throwsError}' for the throwsError. ThrowsError can only be a Boolean.`
-      this[_logger].error(errorDesc)
-      throw new Error(errorDesc)
-    }
-
-    resolve(jsonObj)
+    return filterRule
   }
   
   get filterRules () {
     return this[_filterRules]
+  }
+
+  get shouldThrowError() {
+    return this[_shouldThrowError]
   }
 } 
 
--- microservices/Filter/newModule/base/lib/filter/jsonFilter.new.js
+++ microservices/Filter/newModule/base/lib/filter/jsonFilter.new.js
@@ -1,25 +1,48 @@
 'user strict'
 
 /*
-*
-*
-*
-*
+* This Class is for filtering on the assumption that the payload is a JSON
+*   Object it will receive one filter rule and analyze the payload it
+*   recieves and store whether or not the payload needs to be filtered or
+*   not.
+* @Author: Ben Smith
+* @Date: Sept 9th, 2018
 */
 
 // 'Private' class methods
-const 
+const _exactStringMatch = Symbol('exactStringMatch')
+const _partialMatch = Symbol('partialMatch')
+const _exactNumberMatch = Symbol('exactNumberMatch')
+const _greaterThanMatch = Symbol('greaterThanMatch')
+const _lessThanMatch = Symbol('lessThanMatch')
 
 // 'Private' class variables
-const 
+const _filterMessages = Symbol('filterMessages')
+const _isJSONObjFiltered = Symbol('isJSONObjFiltered')
+const _shouldThrowError = Symbol('shouldThrowError')
 
 class Filter {
 
-  constructor() {
-    this[_isJSONObjFiltered] = true
-    
+  constructor(logger, shouldThrowError) {
+
+    // List of filter rules that the message failed to pass
+    this[_filterMessages] = []
+    // Assume that the message should not be filtered out
+    this[_isJSONObjFiltered] = false
+    // Should an error be thrown if the message is filtered out
+    this[_shouldThrowError] = shouldThrowError
+
   }
 
+  /*
+  * Desc:
+  *   This method checks to see if there is full match with a string
+  * Args:
+  *   data - String - The string in question
+  *   acceptedValues - Array[Strings] The potential strings the data can match with
+  * Throws:
+  *   Error - Any error that the indexOf can throw or other RunTimeExceptions
+  */
   async [_exactStringMatch] (data, acceptedValues) {
     if (acceptedValues.indexOf(data) !== -1) {
       resolve(true)
@@ -28,6 +51,16 @@ class Filter {
     }
   }
 
+  /*
+  * Desc:
+  *   This method like the exact match but can match if a small portion of a possible
+  *     match matchs the data.
+  * Args:
+  *   data - String - The string in question
+  *   acceptedValues - Array[Strings] The potential strings the data can match with
+  * Throws:
+  *   Error - Any error that the indexOf can throw or other RunTimeExceptions
+  */
   async [_partialMatch] (data, acceptedValues) {
     for (const possibleValue in acceptedValues) {
       if (acceptedValues[possibleValue].indexOf(data) !== -1) {
@@ -37,6 +70,16 @@ class Filter {
     resolve(false)
   }
 
+  /*
+  * Desc:
+  *   This method checks to the see if the numbers in the accepted list
+  *     match with with the data.
+  * Args:
+  *   data - Number - The number in question
+  *   acceptedValues - Array[Number] The potential strings the data can match with
+  * Throws:
+  *   Error - Any error that the Number class can throw or other RunTimeExceptions
+  */
   async [_exactNumberMatch] (data, acceptedValues) {
     if (Number(data) === Number(acceptedValues[0])) {
       resolve(true)
@@ -45,6 +88,15 @@ class Filter {
     }
   }
 
+  /*
+  * Desc:
+  *   This method check to see if the passed in number is greater than the accepted value
+  * Args:
+  *   data - Number - The number in question
+  *   acceptedValues - Array[Number] The potential strings the data can match with
+  * Throws:
+  *   Error - Any error that the Number class can throw or other RunTimeExceptions
+  */
   async [_greaterThanMatch] (data, acceptedValues) {
     if (Number(data) > Number(acceptedValues[0])) {
       resolve(true)
@@ -53,6 +105,15 @@ class Filter {
     }
   }
 
+  /*
+  * Desc:
+  *   This method check to see if the passed in number is less than the accepted value
+  * Args:
+  *   data - Number - The number in question
+  *   acceptedValues - Array[Number] The potential strings the data can match with
+  * Throws:
+  *   Error - Any error that the Number class can throw or other RunTimeExceptions
+  */
   async [_lessThanMatch] (data, acceptedValues) {
     if (Number(data) < Number(acceptedValues[0])) {
       resolve(true)
@@ -61,13 +122,28 @@ class Filter {
     }
   }
 
+  /*
+  * Desc:
+  *   This methods analyzes the payload with a particular filter rule and checks to see
+  *     if patload need to be filtered out based on that filter rule.
+  * Args:
+  *   data - Object - The whole JSON payload that is being analyzed
+  *   filterKey - String - The data that is being looked for this is more for human reading
+  *   pathToKey - Array[String] - The path to the data point. Note: this CAN'T point to a JSONArray
+  *   typeOfMatch - String - The type of match that will be compared to the accepted values
+  *   acceptedValues - Array[String or Number] - The accepted values that will be used to determine
+  *                                              if the data will be filtered out or not.
+  * Throws:
+  *   Error - If the path to the key is broken
+  *   Error - If the type of match does not have a case.
+  */
   async processPayload() {
 
-    let data = arguments[0]
-    let pathToKey = arguments[1]
-    let typeOfMatch = arguments[2]
-    let acceptedValues = arguments[3]
-    let throwsError = arguments[4]
+    const data = Object.assign({}, arguments[0])
+    const filterKey = arguments[1]
+    const pathToKey = arguments[2]
+    const typeOfMatch = arguments[3]
+    const acceptedValues = arguments[4]
 
     for (const key in pathToKey) {
       data = data[pathToKey[key]]
@@ -75,142 +151,105 @@ class Filter {
         throw new Error(`The value '${pathToKey[key]}' does not exist at the path '${pathToKey.join(' -> ')}' in the payload`)
       }
     } 
-    let result = true
+
+    let isFiltered = null
     switch (typeOfMatch) {
       case 'exactString':
+        isFiltered = await [_exactStringMatch](data, acceptedValues)
+        if(isFiltered) {
+          this[_filterMessages].push(`The value '${data}' that was encountered for 
+                                      the key '${filterKey}' at the location in the 
+                                      data '${pathToKey.join(' -> ')}' was not an 
+                                      exact match to the accepted string value(s) 
+                                      '${acceptedValues.join(', ')}'`)
+        }
         break
       case 'partial':
+        isFiltered = await [_partialMatch](data, acceptedValues)
+        if(isFiltered) {
+          this[_filterMessages].push(`The value '${data}' that was encountered for 
+                                      the key '${filterKey}' at the location 
+                                      '${pathToKey.join(' -> ')}' was not an partial 
+                                      match to the accepted value(s) 
+                                      '${acceptedValues.join(', ')}'`)
+        }
         break
       case 'exactNumber':
+        isFiltered = await [_exactNumberMatch](data, acceptedValues)
+        if(isFiltered) {
+          this[_filterMessages].push(`The value '${data}' that was encountered for 
+                                      the key '${filterKey}' at the location in the 
+                                      data '${pathToKey.join(' -> ')}' was not equal 
+                                      than the accepted number value 
+                                      '${acceptedValues[0]}'`)
+        }
         break
       case 'greaterThan':
+        isFiltered = await [_greaterThanMatch](data, acceptedValues)
+        if(isFiltered) {
+          this[_filterMessages].push(`The value '${data}' that was encountered for 
+                                      the key '${filterKey}' at the location in the 
+                                      data '${pathToKey.join(' -> ')}' was not greater 
+                                      than the accepted value '${acceptedValues[0]}'`)
+        }
         break
       case 'lessThan':
+        isFiltered = await [_lessThanMatch](data, acceptedValues)
+        if (isFiltered){
+          this[_filterMessages].push(`The value '${data}' that was encountered for 
+                                      the key '${filterKey}' at the location in the 
+                                      data '${pathToKey.join(' -> ')}' was not less 
+                                      than the accepted value '${acceptedValues[0]}'`)
+        }
         break
       default:
-        throw new Error('The \'typeOfMatch\' - \'' + typeOfMatch + '\' in the configuration for the filtering of the key \'' + filterKey + '\' has a misconfigured value')
+        throw new Error(`The 'typeOfMatch' - '${typeOfMatch}' in the 
+                        configuration for the filtering of the key 
+                        '${filterKey}' is not a recognized 
+                        configuration value`)
+    }
+  
+    // If one rule fails then the payload should be filtered out 
+    if (isFiltered && !this[_isJSONObjFiltered]) {
+      this[_isJSONObjFiltered] = true 
     }
 
   }
 
+  /*
+  * Desc:
+  *   This getter method is used to get whether the payload should be filtered or not 
+  *     based on the filter rules it has ued to analyze the payload.
+  * Args:
+  *   N/A
+  * Throws:
+  *   Error - If the Message payload should be filtered out and an error should be thrown.
+  */
   get isJSONObjFiltered() {
     const isJSONObjFiltered = this[_isJSONObjFiltered]
-    this[_isJSONObjFiltered] = true
+
+    // Reset the _isJSONObjFiltered Boolean
+    this[_isJSONObjFiltered] = false
+
+    // If the Message payload should be filtered out and
+    // an error should be thrown.
+    if (isJSONObjFiltered && this[_shouldThrowError]) {
+      const errorDesc = `The data was filtered out due to the following failed filtering rules: ${this[_filterMessages].join('\n')}` 
+
+      // Reset the filterMessages list
+      this[_filterMessages] = []
+
+      this[_logger].error(errorDesc)
+      throw new Error(errorDesc)
+    } else {
+      this[_logger].warning(`The data was filtered out due to the following failed filtering rules: ${this[_filterMessages].join('\n')}`)
+
+      // Reset the filterMessages list
+      this[_filterMessages] = []
+
+    }
     return isJSONObjFiltered
   }
 }
 
 module.exports = Filter
-/*
-* Description:
-*   This promise performs the actual filtering for every key in the Filter microservice configuration.
-* Args:
-*   payload (Obj): This obj is the payload passed to the filter to be filtered.
-*   configJSON (Obj): This obj has the configurations for the Filter microservice.
-*   logger (Logger): This is the logger that is provided by the consumer.
-* Returns:
-*   shouldBeFiltered (Boolean): This Boolean determines whether the message should be filtered or not. It is assumed that it should be filtered
-* Throws:
-*   JSONFilterError (Error): This error is raised if the key does not exist in the payload in the first place or failure to pass all filter rules
-*     is configured to raise an exception.
-* Notes:
-*   From a design perspective if the key doesn't even exist it should have been filtered out further upstream in the first place as oppose to having it being filtered out in this microservice. So an error will be raised
-*   When looking at the values that are encountered in the JSON object the only time that a value can be undefined is when the value does not exist.
-* TODO:
-*   [#1]:
-*     Add the feature to allow multiple comparisons from multiple keys
-*/
-let filterData = (payload, configJSON, logger) => {
-  return new Promise (
-    resolve => {
-      const config = configJSON
-      let resultArray = []
-      let errorArray = []
-      //[#1]
-      for (let filterKey in config) {
-
-        let data = Object.assign({}, payload)
-        let pathToKey = configJSON[filterKey]['location'].slice(0)
-        let typeOfMatch = configJSON[filterKey]['typeOfMatch'].slice(0)
-        let acceptedValues = configJSON[filterKey]['acceptedValues'].slice(0)
-        let throwsError = configJSON['throwsError']
-
-        for (let key in pathToKey) {
-          data = data[pathToKey[key]]
-          if (data === undefined) {
-            throw new JSONFilterError('The value \'' + pathToKey[key] + '\' does not exist at the path \'' + pathToKey.join(' -> ') + '\' in the payload')
-          }
-        }
-
-        switch (typeOfMatch) {
-        case 'exactString':
-          if (acceptedValues.indexOf(data) !== -1) {
-            logger.debug('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was an exact match to the string value \'' + acceptedValues[acceptedValues.indexOf(data)] + '\' from the accepted value(s) \'' + acceptedValues.join(', ') + '\'')
-            resultArray.push(true)
-          } else {
-            errorArray.push('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was not an exact match to the accepted string value(s) \'' + acceptedValues.join(', ') + '\'')
-            resultArray.push(false)
-          }
-          break
-        case 'partial':
-          for (let possibleValue in acceptedValues) {
-            if (acceptedValues[possibleValue].indexOf(data) !== -1) {
-              logger.debug('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was an partial or exact match to the accepted value \'' + acceptedValues[possibleValue] + '\' from the accepted value(s) \'' + acceptedValues.join(', ') + '\'')
-              resultArray.push(true)
-              break
-            }
-            if (parseInt(possibleValue) === (acceptedValues.length - 1)) {
-              errorArray.push('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location \'' + pathToKey.join(' -> ') + '\' was not an partial match to the accepted value(s) \'' + acceptedValues.join(', ') + '\'')
-              resultArray.push(false)
-            }
-          }
-          break
-        case 'exactNumber':
-          if (Number(data) === Number(acceptedValues[0])) {
-            logger.debug('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was equal than the accepted number value \'' + acceptedValues[0] + '\'')
-            resultArray.push(true)
-          } else {
-            errorArray.push('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was not equal than the accepted number value \'' + acceptedValues[0] + '\'')
-            resultArray.push(false)
-          }
-          break
-        case 'greaterThan':
-          if (Number(data) > Number(acceptedValues[0])) {
-            logger.debug('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was greater than the accepted value \'' + acceptedValues[0] + '\'')
-            resultArray.push(true)
-          } else {
-            errorArray.push('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was not greater than the accepted value \'' + acceptedValues[0] + '\'')
-            resultArray.push(false)
-          }
-          break
-        case 'lessThan':
-          if (Number(data) < Number(acceptedValues[0])) {
-            logger.debug('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was less than the accepted value \'' + acceptedValues[0] + '\'')
-            resultArray.push(true)             
-          } else {
-            errorArray.push('The value \'' + data + '\' that was encountered for the key \'' + filterKey + '\' at the location in the data \'' + pathToKey.join(' -> ') + '\' was not less than the accepted value \'' + acceptedValues[0] + '\'')
-            resultArray.push(false)
-          }
-          break
-        default:
-          throw new JSONFilterError('The \'typeOfMatch\' - \'' + typeOfMatch + '\' in the configuration for the filtering of the key \'' + filterKey + '\' has a misconfigured value')
-        }
-      }
-      let finalResult = true
-      resultArray.forEach(function(result) {
-        finalResult = result && finalResult
-      })
-      if (!finalResult && throwsError) {
-        logger.error('The data was filtered out due to the following failed filtering rules: \n' + errorArray.join('\n'))
-        throw new JSONFilterError('Message payload failed to to pass all filter rules')
-      } else {
-        logger.warning('The data was filtered out due to the following failed filtering rules: \n' + errorArray.join('\n'))
-      }
-      resolve(finalResult)
-    }
-  )
-}
-
-module.exports = {
-  filterData: filterData
-}
