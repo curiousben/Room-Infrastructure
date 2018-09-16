4c2dd6815205d45c51fcd73b4d0b0d2672dd3c94
curiousben <benjamindsmith3@gmail.com>
2018-09-16 15:27:33 -0400

    Updated the file structure of the Aggregator

--- microservices/Aggregator/base/scripts/startMicroservice.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-echo "Starting the $MICROSERVICE in a $NODE_ENV environment"
-node ./bin/aggregator.js
--- microservices/Aggregator/base/scripts/testImage.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --name aggregator-test --link RedisBroker:redis-broker -v /home/cortana/aggregator:/opt/aggregator/config/ curiousben/aggregator sleep 99999999
